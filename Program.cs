db0d4c8b78b8c402143a69b5f0e93779f0739ac3
Vladimír Farkaš <asterix5@azet.sk>
2024-12-01 14:04:35 +0100

    Pridane dalsie funkcie pracu s polom plus vzdy sa vytvory nove pole

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Lekcia6DU
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lekcia6_Cvicenie
 {
     internal class Program
     {
@@ -9,32 +11,83 @@
             int[] pole1x = null;
             Console.WriteLine("Pôvodné pole: [" + string.Join(", ", pole1) + "]");
 
-            ZoradeniePola(pole1x);
-            Console.WriteLine("Nové pole: [" + string.Join(", ", pole1) + "]");
+            int[] zoradenePole = ZoradeniePola(pole1);
+            Console.WriteLine("Nové pole zoradené: [" + string.Join(", ", zoradenePole) + "]");
+
+            int[] obratenePole = obratitPole(pole1);
+            Console.WriteLine("Nové pole obrátené: [" + string.Join(", ", obratenePole) + "]");
+
+            int[] odstraneniePrvehoPrvku = odstraniePrvehoPrvku(pole1, 2);
+            Console.WriteLine("Nové pole obrátené: [" + string.Join(", ", odstraneniePrvehoPrvku) + "]");
         }
 
-        public static void ZoradeniePola(int[] pole)
+        public static int[] ZoradeniePola(int[] pole)
         {
             if (pole == null || pole.Length == 0)
             {
                 Console.WriteLine("Zadal si neplatné hodnoty");
-                return;
+                return new int[0];
             }
             int dlzkaPola = pole.Length;
+            int[] zoradenePole = new int[dlzkaPola];
+            Array.Copy(pole, zoradenePole, dlzkaPola);
             for (int i = 0; i < dlzkaPola - 1; i++)
             {
                 for (int j = 0; j < dlzkaPola - i - 1; j++)
                 {
                     // Porovnanie susedných prvkov
-                    if (pole[j] > pole[j + 1])
+                    if (zoradenePole[j] > zoradenePole[j + 1])
                     {
                         // Vymenenie prvkov, ak sú v zlom poradí
-                        int vymenaPrvkov = pole[j];
-                        pole[j] = pole[j + 1];
-                        pole[j + 1] = vymenaPrvkov;
+                        int vymenaPrvkov = zoradenePole[j];
+                        zoradenePole[j] = zoradenePole[j + 1];
+                        zoradenePole[j + 1] = vymenaPrvkov;
                     }
                 }
             }
+            return zoradenePole;
+        }
+        public static int[] obratitPole(int[] pole)
+        {
+            if (pole == null || pole.Length == 0)
+            {
+                Console.WriteLine("Zadal si neplatné hodnoty");
+                return new int[0];
+            }
+            int dlzkaPola = pole.Length;
+            int[] obratenePole = new int[dlzkaPola];
+            for (int i = 0;i < dlzkaPola; i++)
+            {
+                // Výmena prvkov prvý sa vymení s posledným
+                obratenePole[i] = pole[dlzkaPola - i - 1];
+            }
+            return obratenePole;
+        }
+
+        public static int[] odstraniePrvehoPrvku(int[] pole, int index)
+        {
+            if (pole == null || pole.Length == 0)
+            {
+                Console.WriteLine("Zadal si neplatné hodnoty");
+                return new int[0];
+            }
+            else if (index < 0 || index >= pole.Length)
+            {
+                Console.WriteLine("Index je mimo rozsahu. Vraciam pôvodné pole");
+                return pole;
+            }
+            int dlzkaPola = pole.Length;
+            int[] novePole = new int[pole.Length - 1];
+            int indexNovePole = 0;
+            for (int i = 0; i < dlzkaPola; i++)
+            {
+                if (i != index)
+                {
+                    novePole[indexNovePole] = pole[i];
+                    indexNovePole++;
+                }
+            }
+                return novePole;
         }
     }
 }
